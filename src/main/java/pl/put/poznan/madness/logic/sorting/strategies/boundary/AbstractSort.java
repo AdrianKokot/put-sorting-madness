20c06463b0371bd7a2a9274cc48c22f1460f0d4c
mudziok <mudziok@gmail.com>
2022-12-16 19:00:19 +0100

    Remove leftover Javadoc from AbstractSort

--- src/main/java/pl/put/poznan/madness/logic/sorting/strategies/boundary/AbstractSort.java
+++ src/main/java/pl/put/poznan/madness/logic/sorting/strategies/boundary/AbstractSort.java
@@ -2,9 +2,6 @@ package pl.put.poznan.madness.logic.sorting.strategies.boundary;
 
 public abstract class AbstractSort {
 
-  /**
-   * Swap the contents of a[i] and a[j]
-   */
   protected void swap(Object[] a, int i, int j) {
     Object tmp = a[i];
     a[i] = a[j];
