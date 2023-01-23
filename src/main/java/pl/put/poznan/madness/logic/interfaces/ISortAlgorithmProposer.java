65406a6133f840757015136a17ac9cd7ae2a0715
mudziok <mudziok@gmail.com>
2023-01-23 15:21:22 +0100

    Add javadocs ⌨

--- src/main/java/pl/put/poznan/madness/logic/interfaces/ISortAlgorithmProposer.java
+++ src/main/java/pl/put/poznan/madness/logic/interfaces/ISortAlgorithmProposer.java
@@ -3,7 +3,18 @@ package pl.put.poznan.madness.logic.interfaces;
 import java.util.List;
 import pl.put.poznan.madness.logic.sorting.strategies.boundary.SortingAlgorithm;
 
+/**
+ * ISortAlgorithmProposer is an interface for a class that proposes a sorting algorithm for a given
+ * list of data.
+ *
+ * @see pl.put.poznan.madness.logic.sorting.strategies.boundary.SortingAlgorithm
+ */
 public interface ISortAlgorithmProposer {
-
+  /**
+   * Proposes a sorting algorithm for a given list of data.
+   *
+   * @param data the list of data to sort
+   * @return the proposed sorting algorithm
+   */
   <T> SortingAlgorithm proposeAlgorithm(List<T> data);
 }
