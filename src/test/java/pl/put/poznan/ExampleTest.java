0598652772f1a6270c4e6f6cb191b1ebf7016072
mudziok <mudziok@gmail.com>
2023-01-23 12:59:36 +0100

    Add Mockito to dependencies

--- src/test/java/pl/put/poznan/ExampleTest.java
+++ src/test/java/pl/put/poznan/ExampleTest.java
@@ -2,6 +2,9 @@ package pl.put.poznan;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 public class ExampleTest {
   @Test
   public void firstExampleTest() {
